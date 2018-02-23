cb5a3747b2522065518357da23ea67cd3107a3e8
Francieli Lima <fal.franci@gmail.com>
2018-02-22 23:22:51 -0300

    Add first MVVM module

--- app/src/main/java/br/com/francielilima/movies/modules/dashboard/DashboardActivity.kt
+++ app/src/main/java/br/com/francielilima/movies/modules/dashboard/DashboardActivity.kt
@@ -1,13 +1,41 @@
 package br.com.francielilima.movies.modules.dashboard
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import br.com.francielilima.movies.R
 
 class DashboardActivity: AppCompatActivity() {
 
+    var viewModel: DashboardViewModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+
+        registerObservables()
+        fetchMovies()
     }
+
+    //region Private
+
+    private fun registerObservables(){
+        viewModel?.discoverMoviesData?.observe(this, Observer {
+            it?.let {
+                //TODO
+            }
+        })
+    }
+
+    //endregion
+
+    //region ViewModel Calls
+
+    private fun fetchMovies() {
+        viewModel?.fetchDiscoverMovies()
+    }
+    //endregion
 }
--- app/src/main/java/br/com/francielilima/movies/modules/dashboard/DashboardViewModel.kt
+++ app/src/main/java/br/com/francielilima/movies/modules/dashboard/DashboardViewModel.kt
@@ -1,3 +1,25 @@
 package br.com.francielilima.movies.modules.dashboard
 
-class DashboardViewModel
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import android.util.Log
+import br.com.francielilima.movies.utils.network.ApiDataManager
+import br.com.francielilima.movies.utils.network.pokos.DiscoverMovies
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+
+class DashboardViewModel: ViewModel() {
+
+    var discoverMoviesData = MutableLiveData<DiscoverMovies>()
+
+    fun fetchDiscoverMovies() {
+        ApiDataManager.discoverMoviesObservable()
+                ?.subscribeOn(Schedulers.io())
+                ?.observeOn(AndroidSchedulers.mainThread())
+                ?.subscribe({
+                    discoverMoviesData.value = it
+                }, {
+                    Log.e("Error", it.message, it)
+                })
+    }
+}
