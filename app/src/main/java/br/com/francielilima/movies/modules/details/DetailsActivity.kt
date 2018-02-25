4bf760048ad303c43a9fca9001d4971ee767808b
Francieli Lima <fal.franci@gmail.com>
2018-02-25 16:49:30 -0300

    Polish details design

--- app/src/main/java/br/com/francielilima/movies/modules/details/DetailsActivity.kt
+++ app/src/main/java/br/com/francielilima/movies/modules/details/DetailsActivity.kt
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity
 import br.com.francielilima.movies.R
 import br.com.francielilima.movies.models.Movie
 import br.com.francielilima.movies.utils.Constants
+import br.com.francielilima.movies.utils.extensions.text
+import br.com.francielilima.movies.utils.extensions.year
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.view_details.*
 
@@ -45,8 +47,17 @@ class DetailsActivity: AppCompatActivity() {
                 .into(imageViewBackground)
 
         Picasso.with(this)
-                .load("${Constants.URLs.mediumImageURL}${movie.posterPath}")
+                .load("${Constants.URLs.smallImageURL}${movie.posterPath}")
+                .placeholder(R.mipmap.movie_placeholder)
                 .into(imageViewPoster)
+
+        textViewTitle.text = getString(R.string.title_date, movie.title, movie.releaseDate?.year())
+        textViewGenres.text = movie.genres.text()
+        textViewOverview.text = movie.overview
+        textViewRating.text = movie.rating.toString()
+    }
+
+    private fun fetchMovieDetails() {
 
     }
 
